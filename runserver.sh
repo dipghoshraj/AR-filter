8208f57ad582381d0fd1d686e95edba9f309f1d8
dipghosh <dipghoshraj@gmail.com>
2022-02-05 22:06:08 +0530

    server config wth nginx

--- runserver.sh
+++ runserver.sh
@@ -6,4 +6,6 @@ echo "pull complete"
 
 echo "Stop existing pools"
 sudo fuser -k 5000/tcp
-echo "server is running now"
+
+echo "Start new pools"
+gunicorn --certfile=/home/ubuntu/AR-filterfullchain.pem --keyfile=/home/ubuntu/AR-filter/privkey.pem --bind 0.0.0.0:5000 wsgi:app --reload
