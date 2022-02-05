9104f901bd4b2cb4df9263abbf862922ef998733
dipghosh <dipghoshraj@gmail.com>
2022-02-05 16:37:03 +0530

    change server run configurations

--- runserver.sh
+++ runserver.sh
@@ -8,4 +8,5 @@ echo "Stop existing pools"
 sudo fuser -k 5000/tcp
 
 echo "Start new pools"
-gunicorn --certfile=/etc/letsencrypt/live/ar.dliticx.click/fullchain.pem --keyfile=/etc/letsencrypt/live/ar.dliticx.click/privkey.pem --bind 0.0.0.0:5000 wsgi:app --reload
+nohup gunicorn --certfile=/home/ubuntu/AR-filter/fullchain.pem --keyfile=/home/ubuntu/AR-filter/privkey.pem --bind 0.0.0.0:5000 wsgi:app &
+echo "server is running now"
