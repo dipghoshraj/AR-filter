19a8914da0aec9f99b8c6947a8a44724502759fc
dipghosh <dipghoshraj@gmail.com>
2022-02-05 16:40:20 +0530

    serve pool start change

--- runserver.sh
+++ runserver.sh
@@ -6,7 +6,4 @@ echo "pull complete"
 
 echo "Stop existing pools"
 sudo fuser -k 5000/tcp
-
-echo "Start new pools"
-nohup gunicorn --certfile=/home/ubuntu/AR-filter/fullchain.pem --keyfile=/home/ubuntu/AR-filter/privkey.pem --bind 0.0.0.0:5000 wsgi:app &
 echo "server is running now"
