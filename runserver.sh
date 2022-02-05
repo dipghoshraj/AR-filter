fb6c59a1544638a9dcc3770939e197fa42a86e40
dipghosh <dipghoshraj@gmail.com>
2022-02-05 16:14:22 +0530

    ssl addition for domin

--- runserver.sh
+++ runserver.sh
@@ -8,4 +8,4 @@ echo "Stop existing pools"
 sudo fuser -k 5000/tcp
 
 echo "Start new pools"
-gunicorn --bind 0.0.0.0:5000 wsgi:app --reload
+gunicorn --certfile=/etc/letsencrypt/live/ar.dliticx.click/fullchain.pem --keyfile=/etc/letsencrypt/live/ar.dliticx.click/privkey.pem --bind 0.0.0.0:5000 wsgi:app --reload
